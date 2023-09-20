d04ff235e3abfbb2a048913d01b1f5166b661867
bekar guliashvili <beka@bekars-MBP-2.lan>
2023-09-19 22:44:47 -0400

    initial commit

---
+++ custom-validation-annotation/src/main/java/com/example/customvalidationannotation/CustomValidationAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.example.customvalidationannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomValidationAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomValidationAnnotationApplication.class, args);
+    }
+
+}
